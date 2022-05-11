83e2c2a8cfe820df10b68e5274ae1c79839037d7
wnajdek <najdekwojtek1@gmail.com>
2022-05-11 22:47:04 +0200

    2nd commit

---
+++ projekt/app.js
@@ -0,0 +1,19 @@
+const socket = io('ws://localhost:5555');
+
+socket.on('message', text => {
+
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el)
+
+});
+
+document.querySelector('button').onclick = () => {
+
+    const text = document.querySelector('input').value;
+    socket.emit('message', text)
+    
+}
+
+
+
--- projekt/ser.py
+++ projekt/ser.py
@@ -3,20 +3,25 @@ import socketio
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
-    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+    '/': 'projekt/index.html',
+    '/projekt/app.js': 'projekt/app.js'
 })
 
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
+# @sio.event
+# def connect(sid, environ):
+#     print('connect ', sid)
 
-@sio.event
-def my_message(sid, data):
-    print('message ', data)
+# @sio.event
+# def message(sid, data):
+#     print('message ', data)
+#     sio.emit("message", data)
 
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
+# @sio.event
+# def disconnect(sid):
+#     print('disconnect ', sid)
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    #import eventy
+    
+    eventlet.wsgi.server(eventlet.listen(('', 5555)), app)
+    
