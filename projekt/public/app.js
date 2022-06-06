a5348686d8247070afdfd46dc7a5f6d158915613
Radosław Suder <91800103+Endziel@users.noreply.github.com>
2022-06-06 22:50:20 +0200

    Update app.js

--- projekt/public/app.js
+++ projekt/public/app.js
@@ -1,7 +1,7 @@
 import  ThreeJsView  from '/projekt/public/threeJsViewRs.js';
 
 export class AppClient {
-    socket = io('ws://localhost:5555');
+    socket = io('ws://localhost:8000');
 
     Game 
 
