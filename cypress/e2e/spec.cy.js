90a54188552a0d52a8a3cc0f68a65325dc0539f6
wnajdek <najdekwojtek1@gmail.com>
2022-06-12 03:47:38 +0200

    e2e tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,4 +11,32 @@ describe('web socket event handler', () => {
     cy.get('.my-message').should('contain.text', "asdf")
     
   })
+
+  it('testAfterLoad', () => {
+    cy.get('.game-info-message:first').should('contain.text', 'black')
+    cy.get('.game-info-message:nth-child(2)').should('contain.text', 'chess1')
+    
+  })
+
+  it('testResignButton', () => {
+    cy.on('uncaught:exception', (err, runnable) => {
+      // return false to prevent the error from
+      // failing this test
+      return false
+    })
+
+    cy.get('#btn-resign').click()
+    cy.get('.game-info-message:first').should('contain.text', 'RESIGNED')
+    
+  })
+
+  it('testTimerVisible', () => {
+
+    cy.get('.timer-container').should('be.visible')
+    cy.get('.timer-container').should('contain.text', '00:02:00')
+    
+  })
+
+
+
 })
