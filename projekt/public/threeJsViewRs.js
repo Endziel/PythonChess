81508c200c70983a7c1a92d233126f78a84a5858
Radosław Suder <91800103+Endziel@users.noreply.github.com>
2023-12-18 22:22:56 +0100

    vercel

--- projekt/public/threeJsViewRs.js
+++ projekt/public/threeJsViewRs.js
@@ -6,7 +6,7 @@
 // import { DragControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/DragControls.js';
 // import  BuildBoard  from '/projekt/public/buildBoard.js';
 
-import * as THREE from './three/build/three.module.js';
+import * as THREE from '/projekt/three/build/three.module.js';
 import { OrbitControls } from './../three/examples/jsm/controls/OrbitControls.js';
 import { DragControls } from './../three/examples/jsm/controls/DragControls.js';
 import  BuildBoard  from './buildBoard.js';
